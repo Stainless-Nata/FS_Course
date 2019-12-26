b242d7ebfc4e97c22d54d7b620e18fa6f2cfec18
stainless-nata <workingkeo@gmail.com>
2019-12-26 17:48:08 +0200

    trying new conf

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,15 @@
 //keys.js
-if(process.env.NODE_ENV === "production"){
-    //we are in production - return prod keys
-    module.exports = require("./prod")
-}else{
-    //we are in development - return dev keys
-    module.exports = require("./dev")
-}
+// if(process.env.NODE_ENV === 'production'){
+//     //we are in production - return prod keys
+//     module.exports = require('./prod')
+// }else{
+//     //we are in development - return dev keys
+//     module.exports = require('./dev')
+// }
+
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI ,
+    cookieKey: process.env.COOKIE_KEY,
+   };
