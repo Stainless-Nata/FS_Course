cfc7d9f8ab6c224d1776b807e7236dd827779b64
stainless-nata <workingkeo@gmail.com>
2019-12-31 19:55:50 +0200

    try

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 //keys.js
-if(process.env.NODE_ENV == 'production'){
+alert(process.env.NODE_ENV)
+if(process.env.NODE_ENV === 'production'){
     //we are in production - return prod keys
     module.exports = require('./prod')
 } else{
--- config/prod.js
+++ config/prod.js
@@ -2,8 +2,10 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI ,
-    cookieKey: process.env.COOKIE_KEY,
+    faceBookClientID: "",
+    faceBookClientSecret: "",
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY, //can be any string I want
    };
 
-   
+   alert(process.env.GOOGLE_CLIENT_ID)
