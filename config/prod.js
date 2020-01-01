8da1e88c271d6b4fb23b2b24985263f7a4f0de3b
stainless-nata <workingkeo@gmail.com>
2020-01-01 09:25:55 +0200

    adj keys

--- config/prod.js
+++ config/prod.js
@@ -2,8 +2,6 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    faceBookClientID: "",
-    faceBookClientSecret: "",
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY, //can be any string I want
    };
