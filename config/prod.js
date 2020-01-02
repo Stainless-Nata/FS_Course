63df71a635315094b2249b5bf9a5d44d890e07fb
stainless-nata <workingkeo@gmail.com>
2020-01-02 10:42:02 +0200

    tryout

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 //prod.js - production keys here
 module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    googleClientID: process.env.googleClientID,
+    googleClientSecret: process.env.googleClientSecret,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY, //can be any string I want
    };
