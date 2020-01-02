72f8f422d55df78e7e420c0695e3f7acb034d8ae
stainless-nata <workingkeo@gmail.com>
2020-01-02 10:57:42 +0200

    tryout

--- config/prod.js
+++ config/prod.js
@@ -1,15 +1,8 @@
 //prod.js - production keys here
-// module.exports = {
-//     googleClientID: process.env.GOOGLE_CLIENT_ID,
-//     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     mongoURI: process.env.MONGO_URI,
-//     cookieKey: process.env.COOKIE_KEY, //can be any string I want
-//    };
-   module.exports = {
-    googleClientID: '572972703741-4qtvqtkh8rh1mvmp0d12cborjgdpjl5d.apps.googleusercontent.com',
-    googleClientSecret: 'RLIHwJBFBsGyaH-IN-daVDs2',
-    faceBookClientID: '737300880026850',
-    faceBookClientSecret: '9c6a5d19b07d22dc0bdb8367fc1a9e4c',
-    mongoURI: 'mongodb+srv://myuser:PnBhBYTpCRBiAP7G@cluster0-oyryw.mongodb.net/test?retryWrites=true&w=majority',
-    cookieKey: 'sadfasfgdssdghdfsdfad435fdsdfsdd', //can be any string I want
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY, //can be any string I want
    };
+   
